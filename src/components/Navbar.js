4a0b5a4d33a3bd69435d354f2aa5f714fb98b0e2
crunchmaxim <crunchmaxim@gmail.com>
2020-06-20 17:17:17 +0500

    Changed navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {logout} from './../redux/reducers/usersReducer';
 import { Link } from 'react-router-dom';
+import logo from './../images/logo192.png';
+import './styles/Navbar.css';
 
 const Navbar = (props) => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <img className='logo' src={logo}/>
       <Link class="navbar-brand" to="/">Socialapp 2</Link>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
